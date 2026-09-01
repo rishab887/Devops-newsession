ff16547caf90196bd57ad6bbbfdc51a0c4b4beb7
Rishabh Gautam <rishabhgautam668@gmail.com>
2026-09-01 12:24:09 +0530

    Update App.test.js

--- react-app/src/App.test.js
+++ react-app/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getnnn,ByText(/learn react/i);
-  expect(linkElement).not.toBeInTheDocument();
+  const linkElement = screen.get,ByText(/learn react/i);
+  expect(linkElement)toBeInTheDocument();
 });
